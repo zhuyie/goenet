d039fb8be6187a8f8debaebea80517babdb46a07
zhuyie <zhuyie@gmail.com>
2018-02-22 17:44:32 +0800

    tab -> space

--- address.go
+++ address.go
@@ -8,23 +8,23 @@ package goenet
 import "C"
 
 import (
-	"unsafe"
+    "unsafe"
 )
 
 type ENetAddress C.ENetAddress
 
 func (a *ENetAddress) SetHost(h uint) {
-	a.host = C.enet_uint32(h)
+    a.host = C.enet_uint32(h)
 }
 
 func (a *ENetAddress) SetPort(port uint) {
-	a.port = C.enet_uint16(port)
+    a.port = C.enet_uint16(port)
 }
 
 func (a *ENetAddress) SetHostName(hostName string) int {
-	hName := C.CString(hostName)
-	defer C.free(unsafe.Pointer(hName))
-	return int(C.enet_address_set_host((*C.ENetAddress)(a), hName))
+    hName := C.CString(hostName)
+    defer C.free(unsafe.Pointer(hName))
+    return int(C.enet_address_set_host((*C.ENetAddress)(a), hName))
 }
 
 func (a *ENetAddress) HostIp() string {
@@ -37,7 +37,7 @@ func (a *ENetAddress) HostIp() string {
 }
 
 func (a *ENetAddress) Host() string {
-	hostName := C.malloc(50)
+    hostName := C.malloc(50)
     defer C.free(hostName)
     if C.enet_address_get_host((*C.ENetAddress)(a), (*C.char)(hostName), 50) != 0 {
         return ""
